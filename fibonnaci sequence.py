ca07339ba6adc3d0107340d729a3bfd728c6919a
sukjain <100119606+sukjain@users.noreply.github.com>
2022-02-22 17:55:44 +0530

    Add files via upload
    Assignment6

---
+++ fibonnaci sequence.py
@@ -0,0 +1,8 @@
+def fib(a=0, b=1):
+    """Generator that yields Fibonacci numbers. `a` and `b` are the seed values"""
+    while True:
+        yield a
+        a, b = b, a + b
+
+f = fib()
+print(', '.join(str(next(f)) for _ in range(10)))
